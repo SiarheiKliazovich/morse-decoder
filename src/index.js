d4e8f698c68fc25377be9cc718ad07e595aa03fb
SiarheiKliazovich <alex849317@gmail.com>
2022-08-03 19:25:43 +0300

    fear: add decode function

--- src/index.js
+++ src/index.js
@@ -37,10 +37,44 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function sliceIntoChunks(arr, chunkSize) {
+    const res = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+        const chunk = arr.slice(i, i + chunkSize);
+        res.push(chunk);
+    }
+    return res;
 }
 
+
+function splitNChars(txt, num) {
+    const result = [];
+    for (let i = 0; i < txt.length; i += num) {
+      result.push(txt.substr(i, num));
+    }
+    return result;
+  }
+
+function decode(expr) {
+    const ArreySlice = sliceIntoChunks(expr, 10);    
+    const divideByTwo = [];
+    ArreySlice.forEach(el => {   
+        return divideByTwo.push(splitNChars(el, 2))
+    })
+    const morseCode = divideByTwo.map(divideByTwoElem => {
+        morse = []; 
+        divideByTwoElem.forEach(el => {        
+        if (el == 10) morse.push('.')
+        if (el == 11) morse.push('-') 
+        })
+        return morse.join('')
+    })  
+    return morseCode .map( a => a.split(' ')      
+            .map(  b => { if (b === '') return ' '         
+                        return MORSE_TABLE[b] }).join('') ).join('') ;            
+
+};
+     
 module.exports = {
     decode
 }
